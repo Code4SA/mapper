cb83c6284b1597b0665b200a5a3667e5b8876b72
Greg Kempe <greg@kempe.net>
2016-08-03 16:52:06 +0200

    Load data from mapit

--- mapper.js
+++ mapper.js
@@ -4,6 +4,18 @@ var ___thisPath = document.getElementsByTagName('script')[document.getElementsBy
 $(function () {
     "use strict";
 
+    var provinceCodes = {
+        'Eastern Cape': 'EC',
+        'Western Cape': 'WC',
+        'Free State': 'FS',
+        'Gauteng': 'GT',
+        'KwaZulu-Natal': 'KZN',
+        'Limpopo': 'LIM',
+        'Mpumalanga': 'MP',
+        'North West': 'NW',
+        'Northern Cape': 'NC',
+    };
+
     $.widget("capesean.mapper", {
         // default options
         options: {
@@ -31,13 +43,13 @@ $(function () {
             var url;
             switch (this.options.dataType) {
                 case "provinces":
-                    url = "provinces.js";
+                    url = "/areas/PR.geojson";
                     break;
                 case "districts":
-                    url = "districts.js";
+                    url = "/areas/DC.geojson";
                     break;
                 case "municipalities":
-                    url = "municipalities.js";
+                    url = "/areas/MN.geojson";
                     break;
                 case "wards":
                     // province filter not supplied
@@ -46,32 +58,22 @@ $(function () {
                         if (!this.options.allowAllWards)
                             throw ("Error: Ward dataType requires either the province filter or the allowAllWards option enabled");
                         else
-                            url = "wards.js";
+                            url = "/areas/WD.geojson";
                     else {
-                        switch (this.options.province) {
-                            case "Eastern Cape": url = "wards-ec.js"; break;
-                            case "Free State": url = "wards-fs.js"; break;
-                            case "Gauteng": url = "wards-gt.js"; break;
-                            case "KwaZulu-Natal": url = "wards-kzn.js"; break;
-                            case "Limpopo": url = "wards-lp.js"; break;
-                            case "Mpumalanga": url = "wards-mp.js"; break;
-                            case "North West": url = "wards-nw.js"; break;
-                            case "Northern Cape": url = "wards-nc.js"; break;
-                            case "Western Cape": url = "wards-wc.js"; break;
-                            default:
-                                throw ("Error: Invalid province filter: " + this.options.province);
-                        }
+                        url = "/areas/MDB-levels:PR-" + provinceCodes(this.options.province) + "|WD.geojson";
                     }
                     break;
                 default:
                     throw ("Error: Not Implemented dataType option in _getData: " + this.options.dataType);
             }
 
+            url = "https://mapit.code4sa.org" + url + "?generation=2&simplify_tolerance=0.005";
+
             var json = null;
             $.ajax({
                 'async': false,
                 'global': false,
-                'url': ___thisPath + url,
+                'url': url,
                 'dataType': "json",
                 'success': function (data) {
                     json = data;
@@ -82,21 +84,22 @@ $(function () {
         },
 
         // get bound limits by looping through all points
-        _getBounds: function (data) {
+        _extendBounds: function(bounds, geom) {
+            var self = this;
 
-            for (var e = 0; e < data.length; e++) { //entity
-                for (var sh = 0; sh < data[e].shapes.length; sh++) { //shape
-                    var coords = data[e].shapes[sh].coords;
-                    for (var c = 0; c < coords.length; c++) { // coord
-                        var coord = coords[c];
-                        if (this._minLat === null || coord.lat < this._minLat) this._minLat = coord.lat;
-                        if (this._maxLat === null || coord.lat > this._maxLat) this._maxLat = coord.lat;
-                        if (this._minLng === null || coord.lng < this._minLng) this._minLng = coord.lng;
-                        if (this._maxLng === null || coord.lng > this._maxLng) this._maxLng = coord.lng;
-                    }
-                }
+            if (geom.getType() == "MultiPolygon") {
+                geom.getArray().forEach(function(poly) {
+                    self._extendBounds(bounds, poly);
+                });
+            } else {
+                //iterate over the paths
+                geom.getArray().forEach(function(path) {
+                    //iterate over the points in the path
+                    path.getArray().forEach(function(latLng) {
+                        bounds.extend(latLng);
+                    });
+                });
             }
-
         },
 
         // shifts elements in an array
@@ -109,27 +112,29 @@ $(function () {
 
         // prepares the list - filtering, ordering, etc
         _prepareList: function (data) {
+            for (var i = 0; i < data.features.length; i++) {
+                // assign mapit code as id
+                data.features[i].id = data.features[i].properties.codes.MDB;
+            }
 
             // check if no data was supplied as an option
             if (!this.options.data || this.options.data.length == 0) return;
 
             // for each item in the full list
-            for (var m = data.length - 1; m >= 0; m--) {
+            for (var m = data.features.length - 1; m >= 0; m--) {
 
-                var item = data[m];
+                var item = data.features[m];
 
                 // try get it from the options list
-                var option = $.grep(this.options.data, function (e) { return e.id == item.id; })[0];
+                var option = $.grep(this.options.data, function (e) { return e.id == item.properties.codes.MDB; })[0];
 
                 if (!option) {
                     // wasn't supplied, so remove from the list (unless drawAll == true)
                     if (!this.options.drawAll)
-                        data.splice(m, 1);
+                        data.features.splice(m, 1);
                 } else {
-                    // was supplied, so move to end of array (so the lines are drawn on top / last) 
-                    this._moveArrayElement(data, m, data.length);
                     // copy the styles & other properties, eg html info window
-                    $.extend(item, option);
+                    item.properties.options = option;
                 }
             }
         },
@@ -149,9 +154,6 @@ $(function () {
             // prepare the list
             this._prepareList(data);
 
-            // find the min/max coords
-            this._getBounds(data);
-
             // setup map
             var mapOptions = { zoom: 0, center: new google.maps.LatLng(0, 0), mapTypeId: google.maps.MapTypeId.TERRAIN };
             var map = new google.maps.Map(this.element[0], mapOptions),
@@ -160,54 +162,24 @@ $(function () {
             // setup a global infoWindow
             var infoWindow = new google.maps.InfoWindow();
 
-            // drawing.... for each item
-            for (var m = 0; m < data.length; m++) {
-                var item = data[m];
+            var self = this;
+            map.data.setStyle(function(feature) {
+                // use the custom options for this feature, if given
+                var options = feature.getProperty('options') || {};
+                return {
+                    strokeColor: options.strokeColor || self.options.strokeColor,
+                    strokeOpacity: options.strokeOpacity || self.options.strokeOpacity,
+                    strokeWeight: options.strokeWeight || self.options.strokeWeight,
+                    fillColor: options.fillColor || self.options.fillColor,
+                    fillOpacity: options.fillOpacity || self.options.fillOpacity,
+                };
+            });
 
-                // for each shape
-                for (var sh = 0; sh < item.shapes.length; sh++) {
-                    var coords = item.shapes[sh].coords,
-                        polygon,
-                        paths = [];
+            var features = map.data.addGeoJson(data);
 
-                    // foreach point/coord
-                    for (var c = 0; c < coords.length; c++) {
-                        var latLng = new google.maps.LatLng(coords[c].lat, coords[c].lng);
-                        paths.push(latLng);
-                        bounds.extend(latLng);
-                    }
-
-                    // setup the polygon
-                    polygon = new google.maps.Polygon({
-                        paths: paths,
-                        strokeColor: item.strokeColor || this.options.strokeColor,
-                        strokeOpacity: item.strokeOpacity || this.options.strokeOpacity,
-                        strokeWeight: item.strokeWeight || this.options.strokeWeight,
-                        fillColor: item.fillColor || this.options.fillColor,
-                        fillOpacity: item.fillOpacity || this.options.fillOpacity,
-                        html: item.html,
-                        // include these properties so the click event handler can access them
-                        dataType: this.options.dataType,
-                        id: item.id
-                    });
-
-                    // set on map
-                    polygon.setMap(map);
-
-                    // if there's html for a popup, add the listener
-                    if (polygon.html) {
-                        google.maps.event.addListener(polygon, 'click', function (event) {
-                            infoWindow.setContent(this.html);
-                            infoWindow.setPosition(event.latLng);
-                            infoWindow.open(map);
-                        });
-                    }
-
-                    // if there's a click event handler, add the listener
-                    if (this.options.click) {
-                        google.maps.event.addListener(polygon, 'click', this.options.click); // function(event)
-                    }
-                }
+            // extend bounds to include all the features
+            for (var i = 0; i < features.length; i++) {
+                this._extendBounds(bounds, features[0].getGeometry());
             }
 
             // zoom to fit the bounds
