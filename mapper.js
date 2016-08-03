e1880477bea90d81a6c44931aad493220d8fdd42
Greg Kempe <greg@kempe.net>
2016-08-03 18:35:42 +0200

    Change map center

--- mapper.js
+++ mapper.js
@@ -153,7 +153,10 @@ $(function () {
             this._prepareList(data);
 
             // setup map
-            var mapOptions = { zoom: 0, center: new google.maps.LatLng(0, 0), mapTypeId: google.maps.MapTypeId.TERRAIN };
+            var mapOptions = {
+                zoom: 50, 
+                scrollwheel: false,
+            };
             var map = new google.maps.Map(this.element[0], mapOptions),
                 bounds = new google.maps.LatLngBounds();
 
